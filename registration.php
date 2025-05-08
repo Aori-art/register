af5c59356103034e0ccdbc1739f13c40e0a68b2f
Aori-art <liwanagrl@students.nu-lipa.edu.ph>
2025-05-08 15:42:32 +0800

    a

--- registration.php
+++ registration.php
@@ -74,6 +74,7 @@ require_once('classes/functions.php');
 <html lang="en">
 <head>
   <!-- Required meta tags -->
+  <link rel="stylesheet" href="package/dist/sweetalert2.css">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Bootstrap CSS -->
